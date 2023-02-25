d96fb04bbd6b07ba15b229f6ce90a7a519bbabb0
Matheus <math.s.gomes@gmail.com>
2023-02-25 00:32:59 -0300

    Uso errado do service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  ngOnInit() {
+    let ofertas: OfertasService = new OfertasService();
+    console.log(ofertas.getOfertas());
+  }
 }
