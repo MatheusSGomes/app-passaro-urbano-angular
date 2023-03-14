d1cb0368c5c16df76fc66c91914fc9b36fba0cd5
Matheus <math.s.gomes@gmail.com>
2023-03-13 22:24:46 -0300

    Criado o provider de OfertasService

--- src/app/diversao/diversao.component.ts
+++ src/app/diversao/diversao.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-diversao',
   templateUrl: './diversao.component.html',
-  styleUrls: ['./diversao.component.css']
+  styleUrls: ['./diversao.component.css'],
+  providers: [ OfertasService ]
 })
-export class DiversaoComponent {
+export class DiversaoComponent implements OnInit {
+  public ofertas!: any;
 
+  constructor(private ofertasService: OfertasService) { }
+
+  ngOnInit() {
+    this.carregaOfertasPorCategoria();
+  }
+
+  public async carregaOfertasPorCategoria() {
+    const ofertasCategoria = this.ofertasService.getOfertasPorCategoria('diversao');
+    this.ofertas = await lastValueFrom(ofertasCategoria);
+  }
 }
