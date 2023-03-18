fd6f160bc5f92931edff7897c23fe6cbd0a15e62
Matheus <math.s.gomes@gmail.com>
2023-03-18 20:50:25 -0300

    Ajuste acesso API

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,24 +2,24 @@ import { Injectable } from '@angular/core';
 import { Oferta } from './shared/oferta.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { URL_API } from './app.api';
 
 @Injectable()
 export class OfertasService {
 
   public source!: any;
-  private urlApi = 'http://localhost:3000';
 
   constructor(private http: HttpClient) { }
 
   public getOfertas(): Observable<Object> {
-    return this.http.get(this.urlApi + '/ofertas?destaque=true');
+    return this.http.get(`${URL_API}?destaque=true`);
   }
 
   public getOfertasPorCategoria(categoria: string): Observable<Object> {
-    return this.http.get(this.urlApi + `/ofertas?categoria=${categoria}`);
+    return this.http.get(`${URL_API}?categoria=${categoria}`);
   }
 
   public getOfertaPorId(id: number): Observable<object> {
-    return this.http.get(this.urlApi + `/ofertas?id=${id}`);
+    return this.http.get(`${URL_API}?id=${id}`);
   }
 }
