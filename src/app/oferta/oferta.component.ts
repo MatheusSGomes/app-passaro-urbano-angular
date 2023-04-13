a64810e075945d4e48d50da8c4bc88fc0e618e56
Matheus <math.s.gomes@gmail.com>
2023-04-13 16:35:19 -0300

    trabalhando com unsubscribe

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { OfertasService } from '../ofertas.service';
-import { lastValueFrom } from 'rxjs';
+import { Observable, lastValueFrom, interval, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-oferta',
@@ -9,7 +9,10 @@ import { lastValueFrom } from 'rxjs';
   styleUrls: ['./oferta.component.css'],
   providers: [ OfertasService ]
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
+
+  private tempoObservableSubscription!: Subscription;
+  private testeObservableSubscription!: Subscription;
 
   public oferta!: any;
 
@@ -22,5 +25,29 @@ export class OfertaComponent implements OnInit {
     const ofertaPorId = this.ofertasService.getOfertaPorId(this.route.snapshot.params['id']);
     this.oferta = await lastValueFrom(ofertaPorId);
     this.oferta = this.oferta[0];
+    
+    let tempo = interval(2000);
+    this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => console.log(intervalo));
+
+    // observable (observável)
+    let meuObservableTeste = Observable.create((observador: Observer<number>) => {
+      observador.next(1);
+      observador.next(2);
+      // observador.error('Algum erro foi encontrado');
+      observador.complete();
+      observador.next(3); // não executado
+    });
+
+    // observable (observador)
+    this.testeObservableSubscription =  meuObservableTeste.subscribe(
+      (resultado: number) => console.log(resultado),
+      (error: string) => console.error(error),
+      () => console.log('stream de eventos foi finalizada')
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.tempoObservableSubscription.unsubscribe();
+    this.testeObservableSubscription.unsubscribe();
   }
 }
