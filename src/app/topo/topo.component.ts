5bef82b52284715de15119282d27c1eb9862bfe9
Matheus <math.s.gomes@gmail.com>
2023-04-19 21:34:54 -0300

    aplicação do retry com delay

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
 import { Observable } from 'rxjs';
 import { Oferta } from '../shared/oferta.model';
+import { retry } from "rxjs/operators"; 
 
 @Component({
   selector: 'app-topo',
@@ -19,7 +20,10 @@ export class TopoComponent {
 
   public pesquisa(termoDaBusca: string): void {
     this.ofertas = this.ofertasService.pesquisaOfertas(termoDaBusca);
-    this.ofertas.subscribe(
+
+    this.ofertas
+    .pipe(retry({count: 5, delay: 1000}))
+    .subscribe(
       (ofertas: Oferta[]) => console.log(ofertas),
       (error: any) => console.log(error),
       () => console.log('Fluxo de eventos completo')
