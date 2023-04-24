44aced8f618bf00f48c1f49301c788e68e81f1c6
Matheus <math.s.gomes@gmail.com>
2023-04-23 21:56:47 -0300

    adicionado retry

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
 import { Observable, Subject, of } from 'rxjs';
 import { Oferta } from '../shared/oferta.model';
-import { retry, switchMap, debounceTime, distinctUntilChanged } from "rxjs/operators"; 
+import { retry, switchMap, debounceTime, distinctUntilChanged, catchError } from "rxjs/operators"; 
 
 @Component({
   selector: 'app-topo',
@@ -22,6 +22,7 @@ export class TopoComponent implements OnInit {
   ngOnInit(): void {
     this.ofertas = this.subjectPesquisa // retorno ao término de Oferta[]
     .pipe(
+      retry({count: 5, delay: 1000}),
       debounceTime(1000),
       distinctUntilChanged(),
       switchMap((termo: string) => {
@@ -32,7 +33,11 @@ export class TopoComponent implements OnInit {
           return of<Oferta[]>([]);
         }
         
-        return this.ofertasService.pesquisaOfertas(termo)})
+        return this.ofertasService.pesquisaOfertas(termo)}),
+        catchError((err: any) => {
+          console.log(err);
+          return of<Oferta[]>([]);
+        })
     );
 
     this.ofertas.subscribe((ofertas: Oferta[]) => {
