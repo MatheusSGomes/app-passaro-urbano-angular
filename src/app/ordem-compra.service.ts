130b05368bdfcee070258a25215bb1206533ff5a
Matheus <math.s.gomes@gmail.com>
2023-05-12 20:28:49 -0300

    injetar serviço HttpClient

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,6 +1,11 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 import { Pedido } from "./shared/pedido.model";
 
+@Injectable()
 export class OrdemCompraService {
+  constructor (private http: HttpClient) {}
+
   public efetivarCompra (pedido: Pedido): void {
     console.log(pedido);
   }
