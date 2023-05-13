8be4a133dd298fda7709a9eeac2365a8195b85bc
Matheus <math.s.gomes@gmail.com>
2023-05-12 21:50:34 -0300

    conexão com api via post, criação dos headers

--- src/app/ordem-compra.service.ts
+++ src/app/ordem-compra.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpRequest, HttpHeaders } from "@angular/common/http";
 import { Pedido } from "./shared/pedido.model";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators"; 
+import { URL_API } from "./app.api";
 
 @Injectable()
 export class OrdemCompraService {
   constructor (private http: HttpClient) {}
 
-  public efetivarCompra (pedido: Pedido): void {
-    console.log(pedido);
+  public efetivarCompra (pedido: Pedido): Observable<any> {
+    const headers: HttpHeaders = new HttpHeaders();
+    headers.append('Content-type', 'application/json');
+
+    return this.http.post(
+      `${URL_API}/pedidos`,
+      pedido,
+      { headers }
+    ).pipe(map((response: any) => console.log(response)))
   }
 }
